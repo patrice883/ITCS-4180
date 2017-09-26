dfa5e8c090a7db6b82dd4bae042105945061feed
Aileen Benedict <14118258+jelloh@users.noreply.github.com>
2017-09-25 20:30:34 -0400

    Added SeekBars

--- Week-4/InClass04/app/src/main/java/itcs4180/inclass04/MainActivity.java
+++ Week-4/InClass04/app/src/main/java/itcs4180/inclass04/MainActivity.java
@@ -2,39 +2,65 @@ package itcs4180.inclass04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private int passCount = 1;
+    private int passLength = 8;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar seekCount = (SeekBar)findViewById(R.id.seekPassCount);
-        seekCount.setOnSeekBarChangeListener(this);
+        // Update info when either seekbar is moved
+        ((SeekBar)findViewById(R.id.seekPassCount)).setOnSeekBarChangeListener(this);
+        ((SeekBar)findViewById(R.id.seekPassLength)).setOnSeekBarChangeListener(this);
 
+    }
 
+    private void teeHeeSeek(SeekBar seekBar){
+
+        // Separate based on which Seekbar is being used
+        if(seekBar.getId() == R.id.seekPassCount){
+            Log.d("test", "Pass Count Seekbar is being moved~");
+
+            SeekBar seek = (SeekBar)findViewById(R.id.seekPassCount);
+            TextView txt = (TextView)findViewById(R.id.txtShowCount);
+            int min = 1;
+            passCount = seek.getProgress() + min;
+
+            txt.setText(passCount + "");
+        }
+        else if(seekBar.getId() == R.id.seekPassLength){
+            Log.d("test", "Pass Length Seekbar is being moved~");
+
+            SeekBar seek = (SeekBar)findViewById(R.id.seekPassLength);
+            TextView txt = (TextView)findViewById(R.id.txtShowLength);
+            int min = 8;
+            passLength = seek.getProgress() + min;
+
+            txt.setText(passLength + "");
+        }
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-        // Separate based on which Seekbar is being used
-        if(seekBar.getId() == R.id.seekPassCount){
-
-        }
-        else if(seekBar.getId() == R.id.seekPassLength){
-
-        }
+        teeHeeSeek(seekBar);
     }
 
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
-
+        teeHeeSeek(seekBar);
     }
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-
+        teeHeeSeek(seekBar);
     }
 }
