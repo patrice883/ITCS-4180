aa24458c19c65fc186c22c8f13afae107a1b765b
patrice883 <pparson3@uncc.edu>
2018-09-30 22:58:31 -0400

    Update ExampleInstrumentedTest.java

--- Week-6/Homework04/app/src/androidTest/java/com/example/devansh/homework04/ExampleInstrumentedTest.java
+++ Week-6/Homework04/app/src/androidTest/java/com/example/devansh/homework04/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.devansh.homework04;
+package com.example.pparson.homework04;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.devansh.homework04", appContext.getPackageName());
+        assertEquals("com.example.pparson.homework04", appContext.getPackageName());
     }
 }
